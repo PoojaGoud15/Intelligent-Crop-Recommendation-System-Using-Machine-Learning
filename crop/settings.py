a8da1d044e28d2915c6e61736cccf44ebe841cbe
PoojaGoud15 <poojithagoud28@gmail.com>
2026-06-25 13:52:33 +0530

    Fix static files for Render

--- crop/settings.py
+++ crop/settings.py
@@ -123,5 +123,9 @@ STATICFILES_DIRS = [
 
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
